babe680e31a7f5ab468ab61588a622da0355ad5f
alexei andrusceac <alexeiandrusceac@hotmail.com>
2020-07-30 15:04:44 +0300

    Initial commit

---
+++ app/src/main/java/com/stupid/fuckertwo/activities/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package com.stupid.fuckertwo.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stupid.fuckertwo.R
+
+class LoadingActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading_activity);
+    }
+}
